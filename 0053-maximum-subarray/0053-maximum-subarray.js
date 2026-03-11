f7b999bb289a44aa3403a524072626dd28533132
RizakSingh <105098872+RizakSingh@users.noreply.github.com>
2026-03-11 23:19:42 +0530

    Time: 2 ms (70.89%), Space: 63.1 MB (94.48%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -0,0 +1,14 @@
+var maxSubArray = function(nums) {
+
+let current = nums[0]
+let best = nums[0]
+
+for(let i = 1; i < nums.length; i++){
+
+    current = Math.max(nums[i], current + nums[i])
+
+    best = Math.max(best, current)
+}
+
+return best
+};
