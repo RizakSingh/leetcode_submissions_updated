ace4531d54d1c1af994c4ce5c2b03d41d7cd62bc
RizakSingh <105098872+RizakSingh@users.noreply.github.com>
2026-06-03 23:04:49 +0530

    Time: 8 ms (66.43%), Space: 64.2 MB (76.99%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+    var merge = function(intervals) {
+
+    intervals.sort((a,b) => a[0] - b[0]);
+
+    let result = [intervals[0]];
+
+    for(let i = 1; i < intervals.length; i++){
+
+        let last = result[result.length - 1];
+
+        if(intervals[i][0] <= last[1]){
+            last[1] = Math.max(last[1], intervals[i][1]);
+        }else{
+            result.push(intervals[i]);
+        }
+    }
+
+    return result;
+};
