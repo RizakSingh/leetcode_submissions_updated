8a021bf0ef986367563a4a3096aa05b255a63c16
RizakSingh <105098872+RizakSingh@users.noreply.github.com>
2026-06-05 23:28:08 +0530

    Time: 1 ms (88.14%), Space: 54.9 MB (97.38%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let candidate = null 
+    let count = 0 
+    for (let i = 0 ; i<=nums.length-1;i++){
+        if(count==0){
+            candidate = nums[i]
+        }
+        if(candidate==nums[i]){
+            count++
+        }else{
+            count--
+        }
+    }
+    return candidate
+};
