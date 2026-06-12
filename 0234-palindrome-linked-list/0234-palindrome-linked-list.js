4c66e0a0dca23f40caaab7c03e3e96deca289bfa
RizakSingh <105098872+RizakSingh@users.noreply.github.com>
2026-06-12 20:21:02 +0530

    Time: 3 ms (90.35%), Space: 78 MB (53.32%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+
+    let slow = head;
+    let fast = head;
+
+    // Find middle
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    // Reverse second half
+    let prev = null;
+
+    while (slow) {
+        let next = slow.next;
+        slow.next = prev;
+        prev = slow;
+        slow = next;
+    }
+
+    // Compare both halves
+    let left = head;
+    let right = prev;
+
+    while (right) {
+        if (left.val !== right.val) {
+            return false;
+        }
+
+        left = left.next;
+        right = right.next;
+    }
+
+    return true;
+};
