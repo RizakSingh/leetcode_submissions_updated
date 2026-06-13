608f021913c4fa774b767b1b75154010a0c749fa
RizakSingh <105098872+RizakSingh@users.noreply.github.com>
2026-06-13 16:17:04 +0530

    Time: 2 ms (46.71%), Space: 64.9 MB (31.75%) - LeetHub

---
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+var replaceElements = function(arr) {
+                 let maxRight = -1;
+
+        for (let i = arr.length - 1; i >= 0; i--) {
+            let current = arr[i];
+
+            arr[i] = maxRight;
+
+            maxRight = Math.max(maxRight, current);
+        }
+
+        return arr;
+};
