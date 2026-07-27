cdb4277c2592d85e3347655d29db845ab7dbbd7c
RizakSingh <105098872+RizakSingh@users.noreply.github.com>
2026-07-27 18:58:11 +0530

    Time: 0 ms (100%), Space: 54.4 MB (92.91%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -4,17 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function(numbers, target) {
-    let i = 0
-    let j = numbers.length-1
-    let sum = 0 
-for(let r = 0 ; r<=numbers.length-1;r++){
-sum = numbers[i]+numbers[j]
-if(sum===target){
-    return [i+1,j+1]
-}else if(sum>target){
-j--
-}else if(sum<target){
- i++
-}
+    let map = new Map()
+let answer = 0
+    for(let i =0 ;i <numbers.length;i++){
+        let ans = target -numbers[i]
+        if(map.has(ans)){
+return [map.get(ans)+1,i+1]
+        }
+        map.set(numbers[i],i)
     }
 };
