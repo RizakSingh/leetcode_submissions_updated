b126d12c2a4d100c3b88f76bc0c32146b2c7dc02
RizakSingh <105098872+RizakSingh@users.noreply.github.com>
2026-07-28 18:11:13 +0530

    Time: 35 ms (68.2%), Space: 71.9 MB (73.19%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -3,30 +3,31 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    nums.sort((a,b)=>a-b)
-    let arr =[]
-    for (let i = 0 ;i<nums.length-2;i++){
-let left = i+1
+nums.sort((a, b) => a - b)
+    let arr = []
+    for(let i = 0 ; i <nums.length-2;i++){
+           if (i > 0 && nums[i] === nums[i - 1]) continue;
+        let left = i +1
         let right = nums.length-1
-                if(i > 0 && nums[i] === nums[i-1]) continue;
         while(left<right){
-            let sum = nums[i]+nums[left]+nums[right]
-            if(sum===0){
+            let ans = nums[i]+nums[left]+nums[right]
+            if(ans ===0){
                 arr.push([nums[i],nums[left],nums[right]])
-                while(left<right && nums[left]===nums[left+1]){
-                    left++
-                }
-                while(left<right && nums[right]===nums[right-1]){
-                    right--
-                }
+            
+            while(left<right && nums[left]==nums[left+1]){
                 left++
-                right--
-            }else if(sum<0){
-                left++
-            }else{
+            }
+            while(left<right && nums[right]==nums[right-1]){
                 right--
             }
+            right--
+            left++
+        }else if(ans>0){
+            right--
+        }else{
+            left++
         }
+        }
+        }
+        return arr
     }
-    return arr
-};
